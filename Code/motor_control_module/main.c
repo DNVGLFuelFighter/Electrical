ad4005633f1fed77f1d6fe4fcbcf957246456642
Mark Hocevar <markhocevar13@gmail.com>
2016-03-31 16:07:26 +0200

    Speed controller nice response

--- Code/motor_control_module/main.c
+++ Code/motor_control_module/main.c
@@ -16,9 +16,12 @@
 void init(void);
 void pwm_init(void);
 void can_recv(CAN_packet *p, unsigned char mob);
+void set_speed(uint16_t target);
 
-
-uint16_t speed = 0; //10bit
+uint16_t max_speed = 19999-2000;
+uint16_t min_speed = 19999-1024;
+uint16_t tar_speed = 1024; //10bit
+uint16_t old_speed = 1024;
 uint16_t max_cnt = 19999; //Period of pwm (19999 -> 20ms)
 
 int main(void){
@@ -27,7 +30,20 @@ int main(void){
 	RGB_init();
 	RGB_led_on(LED_BLUE);
 	_delay_ms(5000);
-	
+	while(1){
+		if(tar_speed < old_speed){
+			set_speed(tar_speed);
+			old_speed = tar_speed;
+		}else if(tar_speed > old_speed){
+			while(tar_speed > old_speed){	
+				old_speed = old_speed + 10;
+				set_speed(old_speed);	
+				_delay_ms(20);
+			}
+		}
+
+	}
+}
 /*	while(1){
 		RGB_led_off(LED_BLUE);
 		RGB_led_on(LED_GREEN);
@@ -47,24 +63,21 @@ int main(void){
 		RGB_led_off(LED_RED);
 		RGB_led_on(LED_BLUE);
 		break;*/
-	}
-	while(1){}
-}
-
+	
 void init(void){
 	can_init();
 	USART_init(MYUBRR, true);
 	printf("Init complete\n\r");
 	//TODO id of package and mask
-	prepare_rx(0, 14, 0xff, can_recv);
+	prepare_rx(0, 31, 0xff, can_recv);
 }
 
 void can_recv(CAN_packet *p, unsigned char mob){
 	//Parse data from message
 	//TODO do some checking of data so we don't get stupid stuff
-	uint16_t recv = (p->data[0] << 8) | p->data[1] ;
-	speed = max_cnt - (1024 + recv);
-	printf("Recv data %d\n\r", speed);
+	uint8_t recv =  p->data[1] ;
+	tar_speed = 1024 + recv*4;
+	printf("Recv data %d\n\r", tar_speed);
 	//TODO make some stepping so it won't change to harsh
 	//OCR3B = speed;
 }
@@ -86,4 +99,11 @@ void pwm_init(){
 	DDRE |= (1 << DDE4); //set pin as output
 
 	TCCR3B |= (1<<CS31); //start timer with prescaler 1;
+}
+
+
+void set_speed(uint16_t target){	
+	if(((19999 - target) > max_speed) && ((19999 - target) < min_speed)){
+		OCR3B = 19999 - target;
+	}
 }
